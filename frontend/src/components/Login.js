31d37ac18b096ebfb96e99e3503c665211e7a1bf
ErwanDerrien <erwan1derrien@gmail.com>
2021-09-21 13:05:43 -0400

    Retrait partiel des librairies axios et material ui

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {useState} from 'react'
-import axios from 'axios';
 import jwt_decode from "jwt-decode";
-import {TextField, Button, Typography, Grid, DialogContentText, Link} from '@mui/material';
-import { Redirect, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 const Login = ({userInformations, setUserInformations}) => {
@@ -18,31 +15,31 @@ const Login = ({userInformations, setUserInformations}) => {
         const email = formElements.email.value;
         const password = formElements.password.value;
     
-        axios ({
-            method: 'POST',
-            baseURL: 'http://localhost:8080/',
-            url: 'user/login',
-            data: JSON.stringify({ email: email, password: password }),
-            headers: {
-                'content-type': 'application/json',
-            }
-          })
-          .then(function (response) {
-              if (response.data !== ''){
-                sessionStorage.setItem('jwt', `Bearer ${response.data}`);
+        // axios ({
+        //     method: 'POST',
+        //     baseURL: 'http://localhost:8080/',
+        //     url: 'user/login',
+        //     data: JSON.stringify({ email: email, password: password }),
+        //     headers: {
+        //         'content-type': 'application/json',
+        //     }
+        //   })
+        //   .then(function (response) {
+        //       if (response.data !== ''){
+        //         sessionStorage.setItem('jwt', `Bearer ${response.data}`);
 
-                const decodedJWT = jwt_decode(response.data);
-                setUserInformations({
-                    email: email,
-                    role: decodedJWT.role,
-                    loggedIn: true
-                });
-                setErrorMessage();
-              } else {
-                let errorMessage = 'Informations ne correspondent à aucun utilisateur sauvegardé'
-                setErrorMessage(errorMessage);
-              }
-        });
+        //         const decodedJWT = jwt_decode(response.data);
+        //         setUserInformations({
+        //             email: email,
+        //             role: decodedJWT.role,
+        //             loggedIn: true
+        //         });
+        //         setErrorMessage();
+        //       } else {
+        //         let errorMessage = 'Informations ne correspondent à aucun utilisateur sauvegardé'
+        //         setErrorMessage(errorMessage);
+        //       }
+        // });
     }
 
     const deleteLocalUserInfo = () => {
@@ -56,48 +53,56 @@ const Login = ({userInformations, setUserInformations}) => {
     }
  
     if(!userInformations.loggedIn) { 
-        return (
-            <div>
-                <Grid container spacing={10} direction="column" alignItems="center" justifyContent="center" style={{minHeight: '150vh'}}>
-                    <Typography variant="h4" align="center">Connexion</Typography>
+        // return (
+        //     <div>
+        //         <Grid container spacing={10} direction="column" alignItems="center" justifyContent="center" style={{minHeight: '150vh'}}>
+        //             <Typography variant="h4" align="center">Connexion</Typography>
 
-                    <DialogContentText>{errorMessage}</DialogContentText>
-                    <form noValidate autoComplete="off" onSubmit={logUserIn}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <Button type="submit" variant="contained" color="primary" style={{marginTop: "10px"}}>
-                            Envoyer
-                        </Button>
-                    </form>
-                </Grid>
-            </div>
-        )
+        //             <DialogContentText>{errorMessage}</DialogContentText>
+        //             <form noValidate autoComplete="off" onSubmit={logUserIn}>
+        //                 <TextField
+        //                     margin="normal"
+        //                     required
+        //                     fullWidth
+        //                     id="email"
+        //                     label="Email Address"
+        //                     name="email"
+        //                     autoComplete="email"
+        //                     autoFocus
+        //                 />
+        //                 <TextField
+        //                     margin="normal"
+        //                     required
+        //                     fullWidth
+        //                     name="password"
+        //                     label="Password"
+        //                     type="password"
+        //                     id="password"
+        //                     autoComplete="current-password"
+        //                 />
+        //                 <Button type="submit" variant="contained" color="primary" style={{marginTop: "10px"}}>
+        //                     Envoyer
+        //                 </Button>
+        //             </form>
+        //         </Grid>
+        //     </div>
+        // )
+        return (
+            <form class="form" onSubmit="${(evt) => { this.signIn(evt); }}">
+                {/* ${this.successMessage ? html`<div class="form__success entire_column">${this.successMessage}</div>` : '' }
+                ${this.errorMessage ? html`<div class="form__error entire_column">${this.errorMessage}</div>` : '' } */}
+                <label for="u">Username:</label><input id="u" name="id" type="email" required />
+                <label for="p">Password:</label><input id="p" name="password" type="password" required minlength="8" />
+                <button class="entire_column" type="submit">Sign Up</button>
+            </form> );
     } else {
         return (
             <div>
-                <Grid container spacing={10} direction="column" alignItems="center" justifyContent="center" style={{minHeight: '100vh'}}>
+                {/* <Grid container spacing={10} direction="column" alignItems="center" justifyContent="center" style={{minHeight: '100vh'}}>
                     <Typography variant="h4" align="center">Connexion</Typography>
                     <DialogContentText>On dirait que vous êtes déjà connecté, voulez-vous vous déconnecter ?</DialogContentText>
                     <Button variant="contained" color="primary" onClick={deleteLocalUserInfo} style={{marginTop: '5vh'}}>Se Déconnecter</Button>
-                </Grid>
+                </Grid> */}
             </div>
         )
     }
